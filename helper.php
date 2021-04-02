9749351e58da3578986dd8113a8e27c8e8a24cac
Michal Mišo Červeňák <miso@fykos.cz>
2021-04-03 01:51:26 +0200

    Add org request

--- helper.php
+++ helper.php
@@ -10,7 +10,9 @@
 // must be run within Dokuwiki
 use dokuwiki\Extension\Plugin;
 use Fykosak\FKSDBDownloaderCore\FKSDBDownloader;
+use Fykosak\FKSDBDownloaderCore\Requests\EventListRequest;
 use Fykosak\FKSDBDownloaderCore\Requests\ExportRequest;
+use Fykosak\FKSDBDownloaderCore\Requests\OrganizersRequest;
 use Fykosak\FKSDBDownloaderCore\Requests\Request;
 use Fykosak\FKSDBDownloaderCore\Requests\Results\ResultsCumulativeRequest;
 use Fykosak\FKSDBDownloaderCore\Requests\Results\ResultsDetailRequest;
@@ -22,28 +24,32 @@ require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPA
 
 class helper_plugin_fksdownloader extends Plugin {
 
-    const EXPIRATION_FRESH = 0;
-    const EXPIRATION_NEVER = 0x7fffffff;
+    public const EXPIRATION_FRESH = 0;
+    public const EXPIRATION_NEVER = 0x7fffffff;
 
     private FKSDBDownloader $downloader;
 
-    public function downloadExport($expiration, string $qid, array $parameters, int $formatVersion = 2): string {
+    public function downloadExport(int $expiration, string $qid, array $parameters, int $formatVersion = 2): string {
         return $this->tryCache2(new ExportRequest($qid, $parameters, $formatVersion), $expiration);
     }
 
-    public function downloadResultsDetail($expiration, int $contest, int $year, int $series): string {
+    public function downloadResultsDetail(int $expiration, int $contest, int $year, int $series): string {
         return $this->tryCache2(new ResultsDetailRequest($contest, $year, $series), $expiration);
     }
 
-    public function downloadResultsCummulative($expiration, int $contest, int $year, array $series): string {
+    public function downloadResultsCummulative(int $expiration, int $contest, int $year, array $series): string {
         return $this->tryCache2(new ResultsCumulativeRequest($contest, $year, $series), $expiration);
     }
 
-    public function downloadResultsSchoolCummulative($expiration, $contest, $year, $series) {
-        msg('fksdownloader: ' . 'School results is deprecated', -1);
+    public function downloadOrganisers(int $expiration, int $contest, ?int $year): string {
+        return $this->tryCache2(new OrganizersRequest($contest, $year), $expiration);
     }
 
-    public function downloadWebServer($expiration, $path) {
+    public function downloadEventsList(int $expiration, array $eventTypeIds): string {
+        return $this->tryCache2(new EventListRequest($eventTypeIds), $expiration);
+    }
+
+    public function downloadWebServer(int $expiration, string $path): callable {
         $filename = self::getWebServerFilename($path);
 
         return $this->tryCache($filename, $expiration, function () use ($path) {
@@ -83,15 +89,15 @@ class helper_plugin_fksdownloader extends Plugin {
             } catch (SoapFault $e) {
                 msg('fksdbexport: ' . $e->getMessage(), -1);
             }
-            return $this->downloader;
         }
+        return $this->downloader;
     }
 
-    private function tryCache($filename, $expiration, $contentCallback) {
+    private function tryCache(string $filename, int $expiration, callable $contentCallback) {
         $cached = $this->getFromCache($filename, $expiration);
 
         if (!$cached) {
-            $content = call_user_func($contentCallback);
+            $content = $contentCallback();
             if ($content) {
                 $this->putToCache($filename, $content);
             }
@@ -101,7 +107,7 @@ class helper_plugin_fksdownloader extends Plugin {
         }
     }
 
-    private function tryCache2(Request $request, $expiration): string {
+    private function tryCache2(Request $request, int $expiration): string {
         $cached = $this->getFromCache($request->getCacheKey(), $expiration);
 
         if (!$cached) {
@@ -115,7 +121,7 @@ class helper_plugin_fksdownloader extends Plugin {
         }
     }
 
-    private function getFromCache($filename, $expiration) {
+    private function getFromCache($filename, int $expiration) {
         $realFilename = $this->getCacheFilename($filename);
         if (file_exists($realFilename) && filemtime($realFilename) + $expiration >= time()) {
             return io_readFile($realFilename);
@@ -124,7 +130,7 @@ class helper_plugin_fksdownloader extends Plugin {
         }
     }
 
-    private function putToCache($filename, $content): void {
+    private function putToCache(string $filename, string $content): void {
         $realFilename = $this->getCacheFilename($filename);
         io_saveFile($realFilename, $content);
     }
@@ -134,7 +140,7 @@ class helper_plugin_fksdownloader extends Plugin {
         return metaFN($id, '.xml');
     }
 
-    public static function getExportId(string $qid, $parameters): string {
+    public static function getExportId(?string $qid, array $parameters): string {
         $hash = md5(serialize($parameters));
         return $qid . '_' . $hash;
     }
